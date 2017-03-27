44ee1b3151680c24d02f3227c9a0e50250e019a4
XiaochenCui <jcnlcxc@163.com>
2017-03-27 22:27:50 +0800

    remove migrations file update model update .gitignore

--- video/models.py
+++ video/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Video(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    publish_date = models.DateField('date published')
+    publish_date = models.DateField('date published', auto_now_add=True)
     url = models.CharField(max_length=200, unique=True)
